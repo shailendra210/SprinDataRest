b4c576149931c185d13d0a5812934892ceed74fd
thaku <s.b.singh.thakur@gmail.com>
2021-05-10 22:18:10 +0530

    learning spring

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values(101,'Shailendra','Java');
+insert into alien values(102,'Anurag','Android');
+insert into alien values(103,'Mohit','python');
+insert into alien values(104,'Rohit','Java');
+insert into alien values(105,'Sumit','Android');
